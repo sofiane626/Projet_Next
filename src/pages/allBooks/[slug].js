62ea316e35acf58792688718920da348a1aa8904
55975 <55975@etu.he2b.be>
2024-02-02 17:00:52 +0100

    dernier commit

---
+++ src/pages/allBooks/[slug].js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const BookPage = ({ book }) => {
+  // Your component logic here
+  return (
+    <div>
+      <h1>{book.title}</h1>
+      {/* Other components and content */}
+    </div>
+  );
+};
+
+export default BookPage;
